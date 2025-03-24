b1828a74e730bc55ba2029b18bb6854c4bbc2d30
Egor <akinisheve@gmail.com>
2025-03-24 19:30:36 +0300

    Update main.cpp

--- func/main.cpp
+++ func/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
-int main()
+float sum(float n1, float n2)
 {
-    cout << "Hello World!" << endl;
+    cout << "число 1: "; cin >> n1;
+    cout << "число 2: "; cin >> n2;
+    return n1 + n2;
+}
+float raz(float n1, float n2)
+{
+    cout << "Первое число: " << endl; cin >> n1;
+    cout << "Второе число: " << endl; cin >> n2;
+    if (n1 > n2)
+        return n1 - n2;
+    else
+        return n2 - n1;
+}
+
+int main() {
+    float a;
+    float b;
+    cout << sum(a, b) << " Sum" << endl;
+    cout << raz(a, b) << " Raz" << endl;
+    
     return 0;
 }
